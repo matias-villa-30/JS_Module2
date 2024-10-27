35a0c44254e7cc023ac121895ff1feedcb4507c5
villa <matias.villalemes@metropolia.fi>
2024-10-27 11:49:46 +0200

    01 valmis

---
+++ 01_numbers.js
@@ -0,0 +1,8 @@
+let numbers = [];
+for (let i = 0; i < 5 ; i++) {
+            numbers[i] = prompt("Give a number");
+        }
+
+for (let i = numbers.length - 1; i>=0; i--){
+  console.log(numbers[i]);
+}
